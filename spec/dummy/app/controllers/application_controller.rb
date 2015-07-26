7012258a67c9f7a5914d3fa0319cc80284c61e7b
Tim Morton <tim@timothymorton.com>
2015-07-26 15:59:33 -0400

    The spec dummy app needs a user model and controller include

--- spec/dummy/app/controllers/application_controller.rb
+++ spec/dummy/app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
+  include GreenFlag::SiteVisitorManagement
   protect_from_forgery
+
+  def current_user
+    nil
+  end
 end
--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150218171852) do
+ActiveRecord::Schema.define(:version => 20150726195118) do
 
   create_table "green_flag_feature_decisions", :force => true do |t|
     t.integer  "feature_id",      :null => false
@@ -68,5 +68,10 @@ ActiveRecord::Schema.define(:version => 20150218171852) do
 
   add_index "green_flag_site_visitors", ["user_id"], :name => "index_green_flag_site_visitors_on_user_id", :unique => true
   add_index "green_flag_site_visitors", ["visitor_code"], :name => "index_green_flag_site_visitors_on_visitor_code", :unique => true
+
+  create_table "users", :force => true do |t|
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
 end
