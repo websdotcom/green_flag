98cd65e5317b7f816ddd653cb78b59fc207d549f
Tim Morton <tim@timothymorton.com>
2015-07-26 16:45:47 -0400

    Added email column to user in dummy app

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150726195118) do
+ActiveRecord::Schema.define(:version => 20150726204409) do
 
   create_table "green_flag_feature_decisions", :force => true do |t|
     t.integer  "feature_id",      :null => false
@@ -72,6 +72,7 @@ ActiveRecord::Schema.define(:version => 20150726195118) do
   create_table "users", :force => true do |t|
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "email"
   end
 
 end
