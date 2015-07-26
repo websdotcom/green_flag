c3a7588f14dc902169af56c8df1b4c196e6d93fa
Tim Morton <tim@timothymorton.com>
2015-07-26 13:33:03 -0400

    Added better feature specs, and fixed links in views

--- spec/features/admin_spec.rb
+++ spec/features/admin_spec.rb
@@ -1,9 +1,14 @@
-feature 'Main features page' do
+feature 'Editing a feature in the admin system' do
 
-  scenario 'when there are no features' do
+  let(:feature) { FactoryGirl.create(:green_flag_feature) }
+
+  scenario 'view edit page' do
+    code = feature.code
+
     visit '/green_flag/admin/features'
-  end
-
-  
+    expect(page).to have_text(code)
+    click_link code
+    expect(page).to have_text(code)    
+  end  
 
 end
