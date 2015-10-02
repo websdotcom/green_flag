2f85cd836cb95cd90932adeb0a63f333339d8a71
Jason Nappier <jason@webs.com>
2015-10-02 19:19:28 -0400

    Cleanup

--- app/helpers/green_flag/application_helper.rb
+++ app/helpers/green_flag/application_helper.rb
@@ -1,12 +1,6 @@
-#module GreenFlag
+module GreenFlag
   module ApplicationHelper
-    def flash_class(level)
-      case level
-        when :notice then "alert alert-info"
-        when :success then "alert alert-success"
-        when :error then "alert alert-error"
-        when :alert then "alert alert-error"
-      end
-    end
+
   end
-#end
+end
+
