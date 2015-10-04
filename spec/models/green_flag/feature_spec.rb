5bbfdac4003dc8a7ae17469b021db08085bf6ce6
Jason Nappier <jason@webs.com>
2015-10-04 17:38:32 -0400

    Quiet hound down a bit

--- spec/models/green_flag/feature_spec.rb
+++ spec/models/green_flag/feature_spec.rb
@@ -107,7 +107,7 @@ require 'spec_helper'
      end
    end
 
-   describe '#latest_version' do
+   describe "#latest_version" do
      let(:feature) { FactoryGirl.create(:green_flag_feature, version_number: 1) }
 
      subject { feature.latest_version }
@@ -133,31 +133,33 @@ require 'spec_helper'
     end
   end
 
-  describe '#requires_manual_deletion?' do
+  describe "#requires_manual_deletion?" do
     let(:feature)           { GreenFlag::Feature.create(code: 'big_font') }
     let(:feature_decisions) { [] }
 
-    context 'with more than 1,000,000 feature decisions' do
+    context "with more than 1,000,000 feature decisions" do
       it 'returns true' do
         expect(feature_decisions).to receive(:count).and_return(2_000_000)
-        expect(feature).to receive(:feature_decisions).and_return(feature_decisions)
+        expect(feature).to receive(:feature_decisions).
+         and_return(feature_decisions)
 
         expect(feature.requires_manual_deletion?).to be_true
       end
     end
 
-    context 'with fewer than 1,000,000 feature decisions' do
-      it 'returns false' do
+    context "with fewer than 1,000,000 feature decisions" do
+      it "returns false" do
         expect(feature_decisions).to receive(:count).and_return(500_000)
-        expect(feature).to receive(:feature_decisions).and_return(feature_decisions)
+        expect(feature).to receive(:feature_decisions).
+         and_return(feature_decisions)
 
         expect(feature.requires_manual_deletion?).to be_false
       end
     end
   end
 
-  describe 'delete_associated_data' do
-    let(:feature) { GreenFlag::Feature.create(code: 'big_font') }
+  describe "delete_associated_data" do
+    let(:feature) { GreenFlag::Feature.create(code: "big_font") }
 
     before(:each) do
       5.times do
@@ -169,15 +171,15 @@ require 'spec_helper'
       feature.delete_associated_data
     end
 
-    it 'deletes all associated rules' do
+    it "deletes all associated rules" do
       expect(feature.rules.count).to eq 0
     end
 
-    it 'deletes all associated events' do
+    it "deletes all associated events" do
       expect(feature.feature_events.count).to eq 0
     end
 
-    it 'deletes all associated feature decisions' do
+    it "deletes all associated feature decisions" do
       expect(feature.feature_decisions.count).to eq 0
     end
   end
